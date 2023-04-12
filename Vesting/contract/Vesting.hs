e8b86f489c7a45310d16d6d653e3f8bcbd48c937
Rob <kritz.rob@gmail.com>
2023-04-12 22:49:37 +0200

    add vesting contract

---
+++ Vesting/contract/Vesting.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds         #-}
+
+module Vesting where
+
+import Plutus.V2.Ledger.Api         (
+                                    PubKeyHash, ScriptContext,
+                                    POSIXTime, Validator, BuiltinData, TxInfo (txInfoValidRange),
+                                    scriptContextTxInfo, mkValidatorScript
+                                    )
+import Plutus.V2.Ledger.Contexts    (txSignedBy)
+import PlutusTx                     (unstableMakeIsData, compile)
+import PlutusTx.Prelude             (traceIfFalse)
+import Plutus.V1.Ledger.Interval    (contains, from)
+import Prelude                      (Bool, ($), (&&), IO)
+import Utilities                    (wrapValidator, writeValidatorToFile)
+
+data VestingDatum = VestingDatum {
+    beneficiary :: PubKeyHash,
+    deadline :: POSIXTime
+}
+unstableMakeIsData ''VestingDatum
+
+{-# INLINABLE validator #-}
+validator :: VestingDatum -> () -> ScriptContext -> Bool 
+validator dtm _ ctx = traceIfFalse "Can only claim after deadline" deadlinePassed &&
+                        traceIfFalse "Not allowed to claim" isBeneficiary
+        where
+            txInfo :: TxInfo
+            txInfo = scriptContextTxInfo ctx
+
+            deadlinePassed :: Bool
+            deadlinePassed = contains (from $ deadline dtm) (txInfoValidRange txInfo)
+
+            isBeneficiary :: Bool
+            isBeneficiary = txSignedBy txInfo $ beneficiary dtm
+
+wrappedVal :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+wrappedVal = wrapValidator validator
+
+compiledValidator :: Validator
+compiledValidator = mkValidatorScript $$(compile [|| wrappedVal ||])
+
+writeToFile :: IO ()
+writeToFile = writeValidatorToFile "./assets/vesting.plutus" compiledValidator
---
+++ Vesting/test/Vesting_test.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE OverloadedStrings     #-}
+
+module Main where
+
+import Prelude (IO, putStrLn)
+
+main :: IO ()
+main = putStrLn("tests not implemented")
