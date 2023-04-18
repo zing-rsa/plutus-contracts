00897339bf18df0233894e814243067284584caf
Rob <kritz.rob@gmail.com>
2023-04-18 22:06:45 +0200

    update readme; work on zingCoin tests

--- ZingCoin/test/zingcoin_test.hs
+++ ZingCoin/test/zingcoin_test.hs
@@ -1,6 +1,55 @@
 module Main where
 
-import Prelude (IO, putStrLn)
+import Plutus.Model          (mustFail, adaValue, defaultBabbage, testNoErrors, TypedValidator, TypedPolicy (TypedPolicy), toV2, newUser, Tx, Run, UserSpend, mintValue, logError)
+import Test.Tasty            (defaultMain, testGroup)
+import Prelude               (IO, ($), (.), Bool (True), Monoid (mconcat), putStrLn)
+import qualified ZingCoin as OnChain
+import Plutus.V2.Ledger.Api (Value)
+import Control.Monad
 
 main :: IO ()
-main = putStrLn "Not implemeneted"
+main = putStrLn "Not implemented"
+
+-- main :: IO ()
+-- main = do 
+--     defaultMain $ do
+--         testGroup
+--             "Test ZingCoin"
+--             [
+--                 expectFail "Plebs can't mint" plebsCantMind  
+-- --            , expectSucceed "Owner can mint" ownerCanMint
+--             ]
+--         where
+--             expectFail msg = expectSucceed msg . mustFail
+--             expectSucceed = testNoErrors (adaValue 100) defaultBabbage
+
+-- type ZingCoinScript = TypedPolicy OnChain.CoinParams
+
+-- coinScript :: ZingCoinScript
+-- coinScript = TypedPolicy $ toV2 (OnChain.compiledPolicy OnChain.zingCoinParams)
+
+-- consumingTx :: UserSpend -> Value -> Tx
+-- consumingTx usp val = 
+--     mconcat 
+--     [
+--         mintValue coinScript val ?
+--     ]
+
+-- -- ownerCanMint :: Run ()
+-- -- ownerCantMint = do
+-- --     not sure how to create a user with a pre-decided pubkeyhash
+
+
+-- plebsCantMint :: Run ()
+-- plebsCantMint = do
+--     u1 <- newUser $ adaValue 10
+
+--     unless (True) $ logError "ayo"
+
+
+
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -2,21 +2,23 @@
 
 This repo serves as a sandbox for smart contract experiments on the cardano blockchain. Each file represents an idea for a self contained contract, along with unit tests and usage examples.
 
+### Vesting
+Lock a utxo at a script address that only a specified beneficairy can unlock, after a given deadline
+
 ### TokenSwap
 Allow a user to list an NFT for sale, that can be bought by other users as long as they pay the seller the required price. The seller can cancel their sale if the NFT has not been sold. 
 
-### Vesting
-wip - Lock a utxo at a script address that only a specified beneficairy can unlock, after a given deadline
-
-### SimpleGambling
-wip - pay a fee to have a chance at winning a prize(might become raffle)
-
 ### ZingCoin
-wip - minting policy to allow minting of custom native tokens
+Minting policy to allow (inifite, and centralized)minting of a native token called ZingCoin
+
+### ZingCoinParameterized
+Zingcoin except with the script constants applied to the compiled code, by Lucid.
+
+### SimpleRaffle
+wip - pay a fee to have a chance at winning a prize(might become raffle)
 
 ### ZingNFT
 wip - minting policy to allow minting of custom NFT's
-
 
 #### Plutus docs
 cd /app/plutus-pioneer-program 
