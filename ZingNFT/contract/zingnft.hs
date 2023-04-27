61f67caae5e487194fd7082a39d4eea493df978f
Rob <kritz.rob@gmail.com>
2023-04-27 22:28:21 +0200

    wip tokenname check

--- ZingNFT/contract/zingnft.hs
+++ ZingNFT/contract/zingnft.hs
@@ -1,17 +1,20 @@
-{-# LANGUAGE OverloadedStrings #-}
+ {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DataKinds #-}
 
 module ZingNFT where
 
 import Plutus.V2.Ledger.Api (ScriptContext (scriptContextTxInfo), CurrencySymbol, BuiltinByteString, BuiltinData,
-                            TxInfo (txInfoInputs, txInfoData, txInfoOutputs),UnsafeFromData (unsafeFromBuiltinData),
-                            TxInInfo (txInInfoOutRef, txInInfoResolved),TxOut (txOutValue, txOutDatum), TokenName (TokenName),
-                            Value (Value, getValue), fromList, OutputDatum (OutputDatum, OutputDatumHash), Datum (getDatum, Datum))
+                            TxInfo (txInfoInputs, txInfoData, txInfoOutputs, txInfoMint),UnsafeFromData (unsafeFromBuiltinData),
+                            TxInInfo (txInInfoOutRef, txInInfoResolved),TxOut (txOutValue, txOutDatum), TokenName (TokenName, unTokenName),
+                            Value (Value, getValue), fromList, OutputDatum (OutputDatum, OutputDatumHash), Datum (getDatum, Datum), singleton, toData, ToData (toBuiltinData))
 import PlutusTx             (unstableMakeIsData, compile, CompiledCode)
 import Utilities            (wrapPolicy, writeCodeToFile)
-import PlutusTx.Prelude     (traceIfFalse, find, any, Integer, (==), map, Ord ((>=)), (+), (&&))
-import Prelude              (lookup, Bool (False, True), Maybe (Just, Nothing), IO, (.), ($))
+import PlutusTx.Prelude     (traceIfFalse, find, any, Integer, (==), map, Ord ((>=), (<=)), (+), (&&), MultiplicativeSemigroup ((*)), isJust, encodeUtf8)
+import Prelude              (lookup, Bool (False, True), Maybe (Just, Nothing), IO, (.), ($), Semigroup ((<>)), Show (show))
+import Plutus.V1.Ledger.Value (flattenValue)
+import PlutusTx.Builtins (serialiseData)
+import qualified Data.ByteString.Char8
 
 
 data ContractInfo = ContractInfo {
@@ -23,20 +26,22 @@ unstableMakeIsData ''ContractInfo
 
 data ThreadDatum = ThreadDatum {
     mintCount :: Integer,
-    threadIdx :: Integer
+    threadIdx :: Integer,
+    threadCount :: Integer
 }
 unstableMakeIsData ''ThreadDatum
 
 {-# INLINABLE policy #-}
 policy :: ContractInfo -> () -> ScriptContext -> Bool
 policy info _ ctx = traceIfFalse "Doesn't consume a threadtoken"             consumesThread &&
-                      traceIfFalse "Returned thread either missing or invalid" returnsThread
+                    traceIfFalse "Returned thread either missing or invalid" returnsThread &&
+                    traceIfFalse "Max supply reached"                        belowSupply
         where 
             txInfo :: TxInfo
             txInfo = scriptContextTxInfo ctx
             
             threadValue :: Value
-            threadValue = Value $ fromList [(threadToken info, fromList [(TokenName "Thread", 1)])]
+            threadValue = singleton (threadToken info) (TokenName "Thread") 1
 
             consumedThreadDatum :: Maybe ThreadDatum
             consumedThreadDatum = case find (\x -> txOutValue x == threadValue) (map txInInfoResolved $ txInfoInputs txInfo) of 
@@ -47,22 +52,38 @@ policy info _ ctx = traceIfFalse "Doesn't consume a threadtoken"             con
 
             returnedThreadDatum :: Maybe ThreadDatum
             returnedThreadDatum = case find (\x -> txOutValue x == threadValue) (txInfoOutputs txInfo) of 
-                 Just out -> case txOutDatum out of 
-                    OutputDatum d -> unsafeFromBuiltinData $ getDatum d
-                    _           -> Nothing
-                 _              -> Nothing
+                Just out -> case txOutDatum out of 
+                   OutputDatum d -> unsafeFromBuiltinData $ getDatum d
+                   _           -> Nothing
+                _              -> Nothing
 
             consumesThread :: Bool
-            consumesThread = case consumedThreadDatum of
-              Just _  -> True
-              Nothing -> False
+            consumesThread = isJust consumedThreadDatum
             
             returnsThread :: Bool
             returnsThread = case returnedThreadDatum of
               Just rd -> case consumedThreadDatum of 
                     Just cd -> mintCount rd == mintCount cd + 1
-                    _       -> False
+                    _       ->  False
               Nothing -> False
+
+            belowSupply :: Bool
+            belowSupply = case returnedThreadDatum of 
+                Just d -> mintCount d <= maxSupply info
+                _       -> False
+            
+            correctId :: Integer
+            correctId = case returnedThreadDatum of
+                Just d -> (threadIdx d * threadCount d) + mintCount d
+                _      -> 0
+
+            idCorrect :: Bool
+            idCorrect = case flattenValue $ txInfoMint txInfo of 
+                -- [(_, tn, _)] -> unTokenName tn == tokenPrefix info <> serialiseData (toBuiltinData correctId)
+                [(_, tn, _)] -> unTokenName tn == tokenPrefix info <> show correctId
+--               ^^                    
+--              currencySymbol is known to be correct since this contract is running
+                _            -> False
 
 {-# INLINABLE wrappedPolicy #-}
 wrappedPolicy :: BuiltinData -> BuiltinData -> BuiltinData -> ()
