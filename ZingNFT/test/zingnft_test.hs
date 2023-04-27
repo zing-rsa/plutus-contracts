05adae7319eb3b972dae3e70de270c3e9c2531ac
Rob <kritz.rob@gmail.com>
2023-04-27 15:34:00 +0200

    update zingnft tests

--- ZingNFT/test/zingnft_test.hs
+++ ZingNFT/test/zingnft_test.hs
@@ -9,86 +9,92 @@ module Main where
 
 import qualified ZingNFT as Onchain
 import Plutus.Model         (
-                             Tx, Run, logError, newUser, adaValue, testNoErrors, defaultBabbage, mustFail, 
-                             TypedPolicy (TypedPolicy), toV2, mintValue, payToKey, scriptCurrencySymbol, 
-                             submitTx, userSpend, spend, UserSpend, currentTimeRad, validateIn, valueAt
+                             Tx, Run, logError, newUser, adaValue, testNoErrors, defaultBabbage, mustFail,
+                             TypedPolicy (TypedPolicy), toV2, mintValue, payToKey, scriptCurrencySymbol,
+                             submitTx, userSpend, spend, UserSpend, currentTimeRad, validateIn, valueAt, noErrors, waitUntil
                             )
-import Prelude              (IO, ($), Monoid (mconcat), Eq ((==)), (.), Bool (True, False), putStrLn)
-import Plutus.V2.Ledger.Api (TokenName(TokenName), PubKeyHash (PubKeyHash), Value (Value), fromList, POSIXTime (POSIXTime))
+import Prelude              (IO, ($), Monoid (mconcat), Eq ((==)), (.), Bool (False), Semigroup ((<>)), putStrLn)
+import Plutus.V2.Ledger.Api (TokenName(TokenName), PubKeyHash, Value (Value), fromList, POSIXTime (POSIXTime))
 import Test.Tasty           (defaultMain, testGroup)
 import Control.Monad        (unless)
 
-
 main :: IO ()
-main = putStrLn "not impl"
+main = do
+    defaultMain $ do
+        testGroup
+            "Test minting ZingBoi"
+            [
+               expectSucceed "Owner mint"      testAuthMint,
+               expectFail    "Non-owner mint"  testUnAuthMint,
+               expectFail    "Mint after lock" afterLocked
+            ]
+        where
+            expectFail msg = expectSucceed msg . mustFail
+            expectSucceed = testNoErrors (adaValue 100) defaultBabbage
 
--- main :: IO ()
--- main = do
---     defaultMain $ do
---         testGroup
---             "Test minting ZingBoi"
---             [
---             --    expectSucceed "Owner mint"     testAuthMint,
---                expectFail    "Non owner mint" testUnAuthMint
---             ]
---         where
---             expectFail msg = expectSucceed msg . mustFail
---             expectSucceed = testNoErrors (adaValue 100) defaultBabbage
+type ZingPolicy = Onchain.Constants -> TypedPolicy ()
 
--- type ZingPolicy = Onchain.Constants -> TypedPolicy ()
+zingCoinPolicy :: ZingPolicy
+zingCoinPolicy p = TypedPolicy $ toV2 (Onchain.compiledPolicy p)
 
--- zingCoinPolicy :: ZingPolicy
--- zingCoinPolicy p = TypedPolicy $ toV2 (Onchain.compiledPolicy p)
+mintingTx :: Onchain.Constants -> PubKeyHash -> Value -> Tx
+mintingTx c u val =
+    mconcat
+    [
+        mintValue (zingCoinPolicy c) () val,
+        payToKey u val
+    ]
 
--- mintingTx :: Onchain.Constants -> PubKeyHash -> UserSpend -> Value -> Tx
--- mintingTx c u usp val =
---     mconcat
---     [
---         userSpend usp,
---         mintValue (zingCoinPolicy c) () val,
---         payToKey u val
---     ]
+testAuthMint :: Run ()
+testAuthMint = do
+    u1 <- newUser $ adaValue 10
 
--- testUnAuthMint :: Run ()
--- testUnAuthMint = do
---     u1 <- newUser $ adaValue 10
+    let constants = Onchain.Constants u1 "ZingBoi" (POSIXTime 1714116796000)
+    let zingSymbol = scriptCurrencySymbol (zingCoinPolicy constants)
+    let mintVal = Value $ fromList [(zingSymbol, fromList [(TokenName "ZingBoi001", 1)])]
 
---     let constants = Onchain.Constants (PubKeyHash "c1bd6f764d3b68b9f689fbc7ea8027fad5fb190a71caab469cfa8f83") ("ZingBoi") (POSIXTime 1713550450)
+    timeInt <- currentTimeRad 50
+    tx <- validateIn timeInt $ mintingTx constants u1 mintVal
 
---     let zingSymbol = scriptCurrencySymbol (zingCoinPolicy constants)
+    submitTx u1 tx
 
---     let mintVal = Value $ fromList [(zingSymbol, fromList [(TokenName "ZingBoi", 1)])]
+    v1 <- valueAt u1
 
---     usp <- spend u1 $ adaValue 1
+    unless (v1 == mintVal <> adaValue 10) $ logError "Balance at address not correct"
 
---     timeInt <- currentTimeRad 50
+testUnAuthMint :: Run Bool
+testUnAuthMint = do
+    u1 <- newUser $ adaValue 10
 
---     tx <- validateIn timeInt $ mintingTx constants u1 usp mintVal
+    let constants = Onchain.Constants "c1bd6f764d3b68b9f689fbc7ea8027fad5fb190a71caab469cfa8f83" "ZingBoi" (POSIXTime 1714116796000)
 
---     submitTx u1 tx -- submit tx fails 
+    let zingSymbol = scriptCurrencySymbol (zingCoinPolicy constants)
 
---     unless False $ logError "Token not minted"
+    let mintVal = Value $ fromList [(zingSymbol, fromList [(TokenName "ZingBoi001", 1)])]
 
-    -- v1 <- valueAt u1
+    timeInt <- currentTimeRad 50
 
-    -- unless (v1 == Value (fromList [(zingSymbol, fromList [(TokenName "ZingBoi", 1)])])) $ logError "Token not minted"
+    tx <- validateIn timeInt $ mintingTx constants u1 mintVal
 
--- testAuthMint :: Run ()
--- testAuthMint = do
---     u1 <- newUser $ adaValue 10
+    submitTx u1 tx
 
---     let constants = Onchain.Constants u1 (TokenName "ZingBoi") (POSIXTime 1713550450)
+    noErrors
 
---     let zingSymbol = scriptCurrencySymbol (zingCoinPolicy constants)
---     let mintVal = Value $ fromList [(zingSymbol, fromList [(TokenName "ZingBoi", 1)])]
+afterLocked :: Run Bool
+afterLocked = do
+    u1 <- newUser $ adaValue 10
 
---     usp <- spend u1 $ adaValue 1
+    let waitToConsume = POSIXTime 1682601577000
+    
+    let constants = Onchain.Constants u1 "ZingBoi" (POSIXTime 1682601557000)
+    let zingSymbol = scriptCurrencySymbol (zingCoinPolicy constants)
+    let mintVal = Value $ fromList [(zingSymbol, fromList [(TokenName "ZingBoi001", 1)])]
 
---     timeInt <- currentTimeRad 50
---     tx <- validateIn timeInt $ mintingTx constants u1 usp mintVal
+    timeInt <- currentTimeRad 50
+    tx <- validateIn timeInt $ mintingTx constants u1 mintVal
 
---     submitTx u1 tx
+    waitUntil waitToConsume
 
---     v1 <- valueAt u1
+    submitTx u1 tx
 
---     unless (v1 == Value (fromList [(zingSymbol, fromList [(TokenName "ZingBoi", 1)])])) $ logError "Token not minted"
+    noErrors
