4cc17088fb3a4520f75958974702500c698f9e87
Rob <kritz.rob@gmail.com>
2023-06-19 22:08:00 +0200

    work on new approach

---
+++ ZingNFT/contract/threadval.hs
@@ -0,0 +1,119 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module ThreadVal where
+
+import Plutus.V2.Ledger.Api       (ScriptContext (scriptContextTxInfo), OutputDatum (OutputDatum),
+                                  UnsafeFromData (unsafeFromBuiltinData), Datum (getDatum), 
+                                  TxOut (txOutDatum, txOutValue),BuiltinByteString, CurrencySymbol, Value, 
+                                  TxInfo (txInfoOutputs, txInfoMint), TokenName (TokenName), singleton, txInfoOutputs, 
+                                  BuiltinData)
+import PlutusTx.Prelude           (traceIfFalse, find, BuiltinString, encodeUtf8, appendString, appendByteString, (==),
+                                  (+), (<), map)
+import PlutusTx.Builtins          (equalsInteger)
+import PlutusTx                   (unstableMakeIsData, CompiledCode, compile)
+import Plutus.V1.Ledger.Value     (valueOf)
+import Utilities                  (wrapValidator, writeCodeToFile)
+
+data ThreadDatum = ThreadDatum {
+    mintCount :: Integer,
+    threadIdx :: Integer,   
+    threadCount :: Integer
+} deriving Show
+unstableMakeIsData ''ThreadDatum
+
+data ThreadInfo = ThreadInfo {
+    threadToken :: CurrencySymbol, 
+    tokenPolicy :: CurrencySymbol,
+    tokenPrefix :: BuiltinByteString,
+    maxSupply :: Integer
+} 
+unstableMakeIsData ''ThreadInfo
+
+{-# INLINABLE threadValidator #-}
+threadValidator :: ThreadInfo -> ThreadDatum -> () -> ScriptContext -> Bool
+threadValidator info dtm _ ctx = 
+        traceIfFalse "Thread token either not returned or invalid"      returnsThread &&
+        traceIfFalse "Max supply reached"                               belowSupply &&
+        traceIfFalse "Associated NFT mint is either missing or invalid" mintsNFT
+    where
+
+        txInfo :: TxInfo
+        txInfo = scriptContextTxInfo ctx
+
+        threadValue :: Value
+        threadValue = singleton (threadToken info) (TokenName "thread") 1
+
+        returnedThreadDatum :: Maybe ThreadDatum
+        returnedThreadDatum = case find (\x -> txOutValue x PlutusTx.Prelude.== threadValue) (txInfoOutputs txInfo) of 
+            Just out -> case txOutDatum out of 
+               OutputDatum d -> unsafeFromBuiltinData $ getDatum d
+               _           -> Nothing
+            _              -> Nothing
+
+        returnsThread :: Bool
+        returnsThread = case returnedThreadDatum of
+            Just rd ->
+                mintCount rd   PlutusTx.Prelude.== mintCount dtm PlutusTx.Prelude.+ 1 && -- 1 mint per Tx
+                threadIdx rd   PlutusTx.Prelude.== threadIdx dtm     &&
+                threadCount rd PlutusTx.Prelude.== threadCount dtm 
+            _       -> False
+        
+        belowSupply :: Bool
+        belowSupply = mintCount dtm PlutusTx.Prelude.< maxSupply info
+
+        mintsNFT :: Bool -- enforce 1 per Tx
+        mintsNFT = valueOf (txInfoMint txInfo) (tokenPolicy info) 
+            (TokenName (appendByteString (tokenPrefix info) (intToBuiltinByteString $ mintCount dtm PlutusTx.Prelude.+ 1 ))) PlutusTx.Prelude.== 1
+
+{-# INLINABLE wrappedThreadValidator #-}
+wrappedThreadValidator :: BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ()
+wrappedThreadValidator p = wrapValidator (threadValidator $ unsafeFromBuiltinData p)
+
+{-# INLINABLE threadValidatorCode #-}
+threadValidatorCode :: CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ())
+threadValidatorCode = $$(compile [|| wrappedThreadValidator ||])
+
+writeToFile :: IO ()
+writeToFile = writeCodeToFile "./assets/threadval.plutus" threadValidatorCode
+
+--------------------------------------------------
+-- helper
+--------------------------------------------------
+
+{-# INLINEABLE intToBuiltinByteString #-}
+intToBuiltinByteString :: Integer -> BuiltinByteString
+intToBuiltinByteString i = encodeUtf8 $ intToString i
+
+{-# INLINEABLE intToString #-}
+intToString :: Integer -> BuiltinString
+intToString i = foldr appendString "" strings
+  where
+    ints = intToInts i
+    strings = PlutusTx.Prelude.map intToChar ints
+
+{-# INLINEABLE intToInts #-}
+intToInts :: Integer -> [Integer]
+intToInts i
+  | equalsInteger a 0 = [b]
+  | otherwise = intToInts a ++ [b]
+  where
+    (a, b) = divMod i 10
+
+{-# INLINEABLE intToChar #-}
+intToChar :: Integer -> BuiltinString
+intToChar i
+  | equalsInteger i 0 = "0"
+  | equalsInteger i 1 = "1"
+  | equalsInteger i 2 = "2"
+  | equalsInteger i 3 = "3"
+  | equalsInteger i 4 = "4"
+  | equalsInteger i 5 = "5"
+  | equalsInteger i 6 = "6"
+  | equalsInteger i 7 = "7"
+  | equalsInteger i 8 = "8"
+  | equalsInteger i 9 = "9"
+  | otherwise = "0" -- fix this
--- ZingNFT/contract/zingnft.hs
+++ ZingNFT/contract/zingnft.hs
@@ -6,41 +6,25 @@
 
 module ZingNFT where
 
-import Plutus.V2.Ledger.Api       (ScriptContext (scriptContextTxInfo), CurrencySymbol, BuiltinByteString, BuiltinData,
-                                  TxInfo (txInfoInputs, txInfoOutputs, txInfoMint),
+import Plutus.V2.Ledger.Api       (ScriptContext (scriptContextTxInfo), CurrencySymbol, BuiltinData,
+                                  TxInfo (txInfoInputs),
                                   UnsafeFromData (unsafeFromBuiltinData), TxInInfo (txInInfoResolved),
-                                  TxOut (txOutValue, txOutDatum), TokenName (TokenName, unTokenName),
-                                  OutputDatum (OutputDatum), singleton, Value, Datum (getDatum), MintingPolicy, mkMintingPolicyScript, ToData (toBuiltinData))
-import Plutus.V1.Ledger.Value     (flattenValue)
+                                  TxOut (txOutValue), TokenName (TokenName), singleton, 
+                                  Value, MintingPolicy, mkMintingPolicyScript, ToData (toBuiltinData))
 import PlutusTx                   (unstableMakeIsData, compile, CompiledCode, applyCode, makeLift, liftCode)
-import PlutusTx.Prelude           (traceIfFalse, find, Integer, (==), map, Ord ((<=), (<), (>=)), (+), (&&),
-                                   MultiplicativeSemigroup ((*)), isJust, appendByteString, otherwise, divMod, (++), foldr, appendString, encodeUtf8, BuiltinString)
-import Prelude                    (Bool (False), Maybe (Just, Nothing), IO, ($), Show)
+import PlutusTx.Prelude           (traceIfFalse, find, (==), map, isJust, Bool)
+import Prelude                    (IO, ($), Show)
 import Utilities                  (wrapPolicy, writeCodeToFile, writePolicyToFile)
-import PlutusTx.Builtins          (equalsInteger)
 
 data ContractInfo = ContractInfo {
-    threadToken :: CurrencySymbol,
-    maxSupply :: Integer,
-    tokenPrefix :: BuiltinByteString
+    threadToken :: CurrencySymbol
 } deriving Show
 unstableMakeIsData ''ContractInfo
 makeLift ''ContractInfo
 
-data ThreadDatum = ThreadDatum {
-    mintCount :: Integer,
-    threadIdx :: Integer,
-    threadCount :: Integer
-} deriving Show
-unstableMakeIsData ''ThreadDatum
-
 {-# INLINABLE policy #-}
 policy :: ContractInfo -> () -> ScriptContext -> Bool
-policy info _ ctx = traceIfFalse "Doesn't consume a threadtoken"             consumesThread &&
-                    traceIfFalse "Returned thread either missing or invalid" returnsThread  &&
-                    traceIfFalse "Max supply reached"                        belowSupply    &&
-                    traceIfFalse "TokenName not correct"                     idCorrect
-
+policy info _ ctx = traceIfFalse "Doesn't consume a threadtoken" consumesThread
         where
             txInfo :: TxInfo
             txInfo = scriptContextTxInfo ctx
@@ -48,44 +32,8 @@ policy info _ ctx = traceIfFalse "Doesn't consume a threadtoken"             con
             threadValue :: Value
             threadValue = singleton (threadToken info) (TokenName "thread") 1
 
-            consumedThreadDatum :: Maybe ThreadDatum
-            consumedThreadDatum = case find (\x -> txOutValue x == threadValue) (map txInInfoResolved $ txInfoInputs txInfo) of 
-                 Just out -> case txOutDatum out of 
-                    OutputDatum d -> unsafeFromBuiltinData $ getDatum d
-                    _           -> Nothing
-                 _              -> Nothing
-
-            returnedThreadDatum :: Maybe ThreadDatum
-            returnedThreadDatum = case find (\x -> txOutValue x == threadValue) (txInfoOutputs txInfo) of 
-                Just out -> case txOutDatum out of 
-                   OutputDatum d -> unsafeFromBuiltinData $ getDatum d
-                   _           -> Nothing
-                _              -> Nothing
-
             consumesThread :: Bool
-            consumesThread = isJust consumedThreadDatum
-            
-            returnsThread :: Bool
-            returnsThread = case (returnedThreadDatum, consumedThreadDatum) of
-              (Just rd, Just td) -> mintCount rd == mintCount td + 1
-              _       -> False
-
-            belowSupply :: Bool
-            belowSupply = case returnedThreadDatum of
-                Just d -> mintCount d <= maxSupply info
-                _       -> False
-            
-            threadedId :: Integer
-            threadedId = case returnedThreadDatum of
-                Just d -> (threadIdx d * threadCount d) + mintCount d
-                _      -> -1
-
-            idCorrect :: Bool
-            idCorrect = case flattenValue $ txInfoMint txInfo of
-                [(_, tn, _)] -> unTokenName tn == appendByteString (tokenPrefix info) (intToBuiltinByteString threadedId) && 
-                                threadedId < maxSupply info &&
-                                threadedId >= 0
-                _            -> False
+            consumesThread = isJust $ find (\x -> txOutValue x == threadValue) (map txInInfoResolved $ txInfoInputs txInfo)
 
 {-# INLINABLE wrappedPolicy #-}
 wrappedPolicy :: BuiltinData -> BuiltinData -> BuiltinData -> ()
@@ -104,41 +52,3 @@ compiledPolicy i = mkMintingPolicyScript ($$(compile [|| wrappedPolicy ||]) `app
 
 writeTestPolicyToFile :: ContractInfo -> IO ()
 writeTestPolicyToFile i = writePolicyToFile "./assets/zingnft.plutus" (compiledPolicy i)
-
---------------------------------------------------
--- helper
---------------------------------------------------
-
-{-# INLINEABLE intToBuiltinByteString #-}
-intToBuiltinByteString :: Integer -> BuiltinByteString
-intToBuiltinByteString i = encodeUtf8 $ intToString i
-
-{-# INLINEABLE intToString #-}
-intToString :: Integer -> BuiltinString
-intToString i = foldr appendString "" strings
-  where
-    ints = intToInts i
-    strings = map intToChar ints
-
-{-# INLINEABLE intToInts #-}
-intToInts :: Integer -> [Integer]
-intToInts i
-  | equalsInteger a 0 = [b]
-  | otherwise = intToInts a ++ [b]
-  where
-    (a, b) = divMod i 10
-
-{-# INLINEABLE intToChar #-}
-intToChar :: Integer -> BuiltinString
-intToChar i
-  | equalsInteger i 0 = "0"
-  | equalsInteger i 1 = "1"
-  | equalsInteger i 2 = "2"
-  | equalsInteger i 3 = "3"
-  | equalsInteger i 4 = "4"
-  | equalsInteger i 5 = "5"
-  | equalsInteger i 6 = "6"
-  | equalsInteger i 7 = "7"
-  | equalsInteger i 8 = "8"
-  | equalsInteger i 9 = "9"
-  | otherwise = "0" -- fix this
